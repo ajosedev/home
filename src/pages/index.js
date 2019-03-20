216a729150055fb0014c9398921575e154149cbb
Andrew Jose <ajosedev@gmail.com>
2019-03-20 21:40:28 +1100

    Separate index and blog index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,41 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
-import Bio from '../components/bio';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import { rhythm } from '../utils/typography';
+// import Bio from '../components/bio';
+// import Layout from '../components/layout';
+// import SEO from '../components/seo';
+// import { rhythm } from '../utils/typography';
 
-class BlogIndex extends React.Component {
+class Index extends React.Component {
   render() {
-    const { data } = this.props;
-    const posts = data.allMarkdownRemark.edges;
-
     return (
-      <Layout location={this.props.location} title="Today I Learned">
-        <SEO
-          title="TIL"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
+      <main>
+        <div className="grid">
+          <header>
+            <h1>Andrew Jose</h1>
+            <div className="links">
+              <a href="https://github.com/ajosedev">GitHub</a>
+              <a href="https://codepen.io/ajosedev/">CodePen</a>
+              <a href="https://www.linkedin.com/in/ajosedev/">LinkedIn</a>
             </div>
-          );
-        })}
-      </Layout>
+          </header>
+          <p>Web</p>
+          <p>Software</p>
+          <p>Games</p>
+          <p className="developer">Developer</p>
+        </div>
+      </main>
     );
   }
 }
 
-export default BlogIndex;
+export default Index;
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt(format: HTML)
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
       }
     }
   }
