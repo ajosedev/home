8195d800abc26ef58d87472749ab3acc12b95430
Andrew Jose <ajosedev@gmail.com>
2019-03-27 19:33:53 +1100

    Add some simple styling to blogLayout

--- src/components/blogLayout.js
+++ src/components/blogLayout.js
@@ -3,19 +3,13 @@ import { Link } from 'gatsby';
 
 import styles from '../styles/blogLayout.modules.css';
 
-class BlogLayout extends React.Component {
-  render() {
-    const { title, children } = this.props;
-
-    return (
-      <div style={styles}>
-        <header>
-          <h1><Link to={`/`}>{title}</Link></h1>
-        </header>
-        <main>{children}</main>
-      </div>
-    );
-  }
-}
+const BlogLayout = ({ children, title }) => (
+  <div style={styles}>
+    <header>
+      <h1><Link to={`/`}>{title}</Link></h1>
+    </header>
+    <main>{children}</main>
+  </div>
+);
 
 export default BlogLayout;
