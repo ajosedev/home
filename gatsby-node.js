c8f0a4064c280473f47d48d761405652f8af9e83
Andrew Jose <ajosedev@gmail.com>
2019-03-30 15:58:23 +1100

    Add prefix to TIL posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -56,11 +56,16 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({
+      node,
+      getNode,
+      basePath: 'til/',
+    });
+
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: `til${value}`,
     })
   }
 }
