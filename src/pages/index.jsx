d237a8156c757675094017c526cca52f769d1d9e
Andrew Jose <ajosedev@gmail.com>
2019-06-29 21:54:26 +1000

    Move index styles to own page
    Optimise background image too

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,98 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { css } from '@emotion/core';
 
 import SEO from '../components/SEO';
+import * as styles from './index.styles';
 
-// TODO - get smaller image/optimise image
 // TODO - icons for links (mobile only?)
-// TODO - fix loading white screen
 // TODO - make colour vars
 // TODO - tests
-const mainStyles = css`
-  background-color: #29385c; // fallback
-  background:
-    linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0)
-    ),
-    url('/bg.jpg');
-  background-size: cover;
-  background-position: center;
-  min-height: 100vh;
-  height: 100%;
-  width: 100%;
-  font-family: 'News Cycle', sans-serif;
-  color: #fff;
-  text-transform: uppercase;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  > div {
-    min-width: 0;
-  }
-`;
-
-const headerStyles = css`
-  @keyframes colourFade {
-    0% {
-      color: #bbb;
-    }
-    100% {
-      color: #000;
-    }
-  }
-
-  animation: 2s colourFade forwards;
-  mix-blend-mode: screen;
-  display: inline-block;
-  border-radius: 2px;
-  background-color: white;
-  padding: 0.2em 0.2em;
-  --fluid-type-min-size: 2.1;
-  --fluid-type-max-size: 4;
-
-  h1 {
-    margin: 0;
-    font-weight: 800;
-  }
-`;
-
-const roleStyles = css`
-  margin: 2em 0 1.5em;
-  font-weight: 800;
-  --fluid-type-min-size: 1.8;
-  --fluid-type-max-size: 2.5;
-
-  p:last-child {
-    font-size: 1.5em;
-  }
-`;
-
-const navStyles = css`
-  ul {
-    display: grid;
-    grid-gap: 0.2em 2em;
-    grid-template-columns: repeat(auto-fit, minmax(100px, max-content));
-    text-transform: initial;
-    --fluid-type-min-size: 1.6;
-    --fluid-type-max-size: 2;
-  }
-  li {
-
-  }
-  a {
-    color: #fff;
-  }
-  a:hover {
-    color: #ddd;
-    mix-blend-mode: overlay;
-  }
-`;
-
+// TODO - fade everything in at same time as text fades in? or do some stagger
+// TODO - better TIL/rest of page styling styling
 const Index = () => (
-  <div>
+  <main css={styles.main}>
     <SEO
       keywords={[
         'ajosedev',
@@ -102,35 +20,34 @@ const Index = () => (
         'webdev',
       ]}
     />
-    <main css={mainStyles}>
-      <div>
-        <header className="fluid-type" css={headerStyles}>
-          <h1>Andrew</h1>
-          <h1>Jose</h1>
-        </header>
-        <div className="fluid-type" css={roleStyles}>
-          <p>Full Stack</p>
-          <p>Developer</p>
-        </div>
-        <nav css={navStyles}>
-          <ul className="fluid-type">
-            <li>
-              <a href="https://github.com/ajosedev">GitHub</a>
-            </li>
-            <li>
-              <a href="https://codepen.io/ajosedev/">CodePen</a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/ajosedev/">LinkedIn</a>
-            </li>
-            <li>
-              <Link to="/til">TIL</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      <header className="fluid-type" css={styles.header}>
+        <h1>Andrew</h1>
+        <h1>Jose</h1>
+      </header>
+      <div className="fluid-type" css={styles.role}>
+        <p>Full Stack</p>
+        <p>Developer</p>
       </div>
-    </main>
-  </div>
+      <nav css={styles.nav}>
+        <ul className="fluid-type">
+          {/* TODO - component this */}
+          <li>
+            <a href="https://github.com/ajosedev">GitHub</a>
+          </li>
+          <li>
+            <a href="https://codepen.io/ajosedev/">CodePen</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/ajosedev/">LinkedIn</a>
+          </li>
+          <li>
+            <Link to="/til">TIL</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </main>
 );
 
 export default Index;
---
+++ src/pages/index.styles.js
@@ -0,0 +1,94 @@
+import { css } from '@emotion/core';
+
+export const main = css`
+  background-color: #29385c; // fallback
+  background:
+    linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0)
+    ),
+    url('/bg.jpg');
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  font-family: 'News Cycle', sans-serif;
+  color: #fff;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > div {
+    min-width: 0;
+  }
+
+  @media (min-width: 45em) {
+    background:
+      linear-gradient(
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0)
+      ),
+      url('/bg-large.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+`;
+
+export const header = css`
+  @keyframes colourFade {
+    0% {
+      color: #bbb;
+    }
+    100% {
+      color: #000;
+    }
+  }
+
+  animation: 2s colourFade forwards;
+  mix-blend-mode: screen;
+  display: inline-block;
+  border-radius: 2px;
+  background-color: white;
+  padding: 0.2em 0.2em;
+  --fluid-type-min-size: 2.1;
+  --fluid-type-max-size: 4;
+
+  h1 {
+    margin: 0;
+    font-weight: 800;
+  }
+`;
+
+export const role = css`
+  margin: 2em 0 1.5em;
+  font-weight: 800;
+  --fluid-type-min-size: 1.8;
+  --fluid-type-max-size: 2.5;
+
+  p:last-child {
+    font-size: 1.5em;
+  }
+`;
+
+export const nav = css`
+  ul {
+    display: grid;
+    grid-gap: 0.2em 2em;
+    grid-template-columns: repeat(auto-fit, minmax(100px, max-content));
+    text-transform: initial;
+    --fluid-type-min-size: 1.6;
+    --fluid-type-max-size: 2;
+  }
+  li {
+
+  }
+  a {
+    color: #fff;
+  }
+  a:hover {
+    color: #ddd;
+    mix-blend-mode: overlay;
+  }
+`;
